23025f074122375ce0e82fe668d5a7db075fc32c
Dheiver Francisco Santos <165256830+dheiver2@users.noreply.github.com>
2025-01-09 18:54:13 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+// src/app.js
 require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
@@ -6,3 +7,21 @@ const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Configurações
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middlewares
+app.use(helmet());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Rotas
+app.use('/', routes);
+
+// Inicialização do servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
